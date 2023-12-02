e9cff0c8f1e9cb55a92f891b273024abf433c68e
Alex Cardoso <alex.cfreire@al.infnet.edu.br>
2023-12-02 15:46:35 -0300

    Atualizando página de Detalhes

--- EletrodomesticosAppWeb/Models/Eletrodomestico.cs
+++ EletrodomesticosAppWeb/Models/Eletrodomestico.cs
@@ -1,14 +1,29 @@
-﻿namespace EletrodomesticosAppWeb.Models;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EletrodomesticosAppWeb.Models;
 
 
 public class Eletrodomestico
 {
     public int EletrodomesticoId { get; set; }
     public string Nome { get; set; }
+    public string NomeSlug => Nome.ToLower().Replace(" ", "-");
+
     public string Descricao { get; set; }
+
+    [DisplayName(displayName:"Preço")]
+    [DataType(DataType.Currency)]
     public double Preco {  get; set; }
+
     public string ImagemUri { get; set; }
+
+    [DisplayName(displayName: "Frete Grátis:")]
     public bool FreteGratis { get; set; }
+    public string FreteGratisFormatado => FreteGratis ? "Sim" : "Não";
+
+    [DisplayName(displayName: "Disponível em:")]
+    [DisplayFormat(DataFormatString ="{0:MMMM \\de yyyy}")]
     public DateTime DataCadastro { get; set;}
 
 }
